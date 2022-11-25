9e7a749af8441389895906e81e969b215efa0a34
NOID1622 <112671264+NOID1622@users.noreply.github.com>
2022-11-25 10:21:39 +0700

    Add files via upload

---
+++ 1 yang benar.py
@@ -0,0 +1,6 @@
+a =str(input("masukkan nama mahasiswa :"))
+b =str(input("masukkan NIM-nya :"))
+if b [0:2]=="71" and int(b[2:4]) <=22 and int(b [2:4]) >=20:  
+ print(f'{a},merupakan mahasiswa informatika angkatan 20 hingga 22')
+else:
+    print(f'{a},bukan merupakan mahasiswa informatika angkatan 20 hingga 22')
